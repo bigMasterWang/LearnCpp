5c56f16270b40ecb462fd9f42eab23bc8d41ff9b
wangxiaofeng01 <wangxiaofeng01@corp.netease.com>
2026-05-09 15:43:22 +0800

    13章13节 结束

--- Chapter_13/Chapter_13_13/chapter_13_02.cpp
+++ Chapter_13/Chapter_13_13/chapter_13_02.cpp
@@ -1,5 +1,6 @@
 #include "Chapter_13/Chapter_13_13/chapter_13.h"
 #include <iostream>
+#include <utility>
 
 
 
@@ -21,10 +22,56 @@ void print(Pair<T, U>& p)
 }
 
 
+template <typename T, typename U>
+struct Pair2
+{
+	T a{};
+	U b{};
+};
+
+
+struct Point
+{
+	int a{};
+	int b{};
+};
+
+
+template <typename T>
+void print2(T& p)
+{
+	std::cout << "[ " << p.a << " , " << p.b << " ]" << std::endl;
+}
+
+
+
+template <typename T, typename U>
+void print3(std::pair<T, U>& p)
+{
+	std::cout << "[ " << p.first << " , " << p.second << " ]" << std::endl;
+}
+
+void test_make()
+{
+	std::cout << "hello test_make" << std::endl;
+}
+
 namespace ch_13_02 {
 	void run()
 	{
 		Pair<int, double> p1{ 1,2.3 };
 		print(p1);
+
+		Pair2<int, double> p2{ 3,4.5 };
+		Point p3{ 6,7 };
+		print2(p2);
+		print2(p3);
+
+		std::pair<int, double> p4{ 8,9.0 };	
+		std::pair<float, double> p5{ 10.1f, 11.2 };
+		std::pair<std::string, std::string> p6{ "hello", "world" };	
+		print3(p4);
+		print3(p5);
+		print3(p6);
 	}
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include <iostream>
 
 
+/*
+决定编译哪些cpp是构建系统的事情，而不是编译器的事情
+头文件只是提供函数声明
+*/
+void test_make();
 
 int main()
 {
 	ch_13_01::run();
 	std::cout << "====================" << std::endl;
 	ch_13_02::run();
+	test_make();
 	return 0;
 }
